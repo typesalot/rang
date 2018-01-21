28a96b48abc3f98f7d92bbe390e5d3cfb970c921
agauniyal <agauniyal@outlook.com>
2018-01-21 19:13:22 +0530

    Add tests for cerr/clog

--- test/visualTest.cpp
+++ test/visualTest.cpp
@@ -5,9 +5,20 @@ using namespace rang;
 
 int main()
 {
-    setControlMode(control::forceColor);  // For appveyor terminal
+    // Forcing color for online CI terminals
+    setControlMode(control::forceColor);
     cout << endl
          << style::reset << bg::green << fg::gray
          << "If you're seeing green background, then rang works!"
+         << style::reset << endl;
+
+    cerr << endl
+         << style::reset << bg::red << fg::gray
+         << "If you're seeing red background, then rang works!"
+         << style::reset << endl;
+
+    clog << endl
+         << style::reset << bg::blue << fg::gray
+         << "If you're seeing blue background, then rang works!"
          << style::reset << endl;
 }
